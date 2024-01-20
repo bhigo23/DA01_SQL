e60ff0f2d345c4f1d424bd6dfce95e03b5910169
Bao Duong <61047721+bhigo23@users.noreply.github.com>
2024-01-20 12:45:15 +0700

    Update DAY15_baitap.sql

--- DAY15_baitap.sql
+++ DAY15_baitap.sql
@@ -73,3 +73,54 @@ FROM tweets;
 /*anh giai thich giup em cau len ROWS BETWEEN 2 PRECEDING AND CURRENT ROW voi a*/
 
 --baitap6
+SELECT 
+  COUNT(merchant_id) AS payment_count 
+FROM (
+SELECT 
+  merchant_id,
+  credit_card_id,
+  transaction_timestamp,
+  LAG(transaction_timestamp) OVER(
+    PARTITION BY merchant_id, credit_card_id, amount
+     ORDER BY transaction_timestamp, credit_card_id) AS next_time,
+  EXTRACT( EPOCH FROM 
+    transaction_timestamp - (LAG(transaction_timestamp) OVER(
+      PARTITION BY merchant_id, merchant_id, credit_card_id, amount
+      ORDER BY transaction_timestamp)) )/60 gap_time,
+  amount
+FROM transactions) sub
+WHERE gap_time <= 10
+
+--baitap7
+SELECT 
+  category,
+  product,
+  total_spend
+FROM (
+SELECT 
+  category, 
+  product, 
+  SUM(spend) AS total_spend,
+  RANK() OVER (
+    PARTITION BY category 
+    ORDER BY SUM(spend) DESC) AS rank 
+FROM product_spend
+WHERE EXTRACT(YEAR FROM transaction_date) = 2022
+GROUP BY category, product) sub
+WHERE rank <= 2 
+
+--baitap8
+SELECT 
+  artist_name,
+  rank_
+FROM (
+SELECT 
+  a.artist_name,
+  DENSE_RANK() OVER (
+    ORDER BY COUNT(b.song_id) DESC) AS rank_
+FROM artists AS a
+JOIN songs AS b ON a.artist_id = b.artist_id
+JOIN global_song_rank AS c ON b.song_id = c.song_id
+WHERE rank <= 10
+GROUP BY a.artist_name) sub
+WHERE rank_ <= 5
